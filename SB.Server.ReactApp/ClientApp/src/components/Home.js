5e8eb61dc863a26aa5f9cb8c84a7466be6f5a440
patrickpetropoulos <33436629+patrickpetropoulos@users.noreply.github.com>
2022-10-10 20:46:12 -0400

    another small change to test pipeline

--- SB.Server.ReactApp/ClientApp/src/components/Home.js
+++ SB.Server.ReactApp/ClientApp/src/components/Home.js
@@ -6,8 +6,7 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, Patrick's world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <h1>Welcome to Patrick's world famous React App!</h1>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
