4562da0105c4f206923b25c1fa0df7269ae452d6
patrickpetropoulos <patrickpetropoulos@protonmail.com>
2023-02-03 22:41:34 -0500

    Adds comment to HealthController, Fixes AB#20

--- SB.Server.App.Api/Controllers/HealthController.cs
+++ SB.Server.App.Api/Controllers/HealthController.cs
@@ -3,12 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SB.Server.App.Api.Controllers;
 
+/// <summary>
+/// Class to handle Health of Controllers
+/// </summary>
 [Route( "api/[controller]" )]
 [ApiController]
 [ApiVersionNeutral]
 [AllowAnonymous]
 public class HealthController : ControllerBase
 {
+	/// <summary>
+	/// Checks the health of Controller
+	/// </summary>
+	/// <remarks></remarks>
+	/// <response code="200"></response>
 	[HttpGet]
 	[Route( "ping" )]
 	public IActionResult Ping()
